ca0981a8590ea2d04486d015a04e437384ae89c7
Bigesto <u.baratelli@gmail.com>
2024-11-10 15:55:44 +0100

    Creation of character and rotate command.

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
-
-
-    # print(f"""
-    #     Starting asteroids!
-    #     Screen width: {SCREEN_WIDTH}
-    #     Screen height: {SCREEN_HEIGHT}"""
-    #     )
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
